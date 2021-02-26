833a328afa84751d85eddb2e1f26260fb56c63ca
Daniel Amarante <daniel.amarante@truecaller.com>
2021-02-26 09:18:44 +0100

    ULTIMATO

---
+++ main.js
@@ -0,0 +1,18 @@
+const renderDate = (countDownDate) => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+}
+
+const countDownDate = new Date("Mar 8, 2021").getTime();
+renderDate(countDownDate)
+
+setInterval(() => {
+  renderDate(countDownDate)
+}, 1000);
